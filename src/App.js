dd788e9ac5cf625dc3dc4f5c0f9c6d03594012e1
Nathan <nathanbark4@gmail.com>
2023-01-18 16:19:11 +0200

    serch function and styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { robots } from "./robots";
 import Cardlist from "./CardList";
+import SearchBox from './SearchBox';
 
 function App() {
+  const [currentRobots] = useState(robots);
+  const [searchField, setSearchField ]= useState('');
+
+  const onSearchChange = (e) => {
+    setSearchField(e.target.value);
+  }
+
+  const filteredRobots = currentRobots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase())
+  })
+
   return (
     <div className="tc">
-      <h1>Robofriends</h1>
-      <Cardlist robots={robots} />;
+      <h1 className="f1 ">Robofriends</h1>
+      <SearchBox searchField={searchField} searchChange={onSearchChange}/>
+      <Cardlist robots={filteredRobots} />;
     </div>
   );
 }
