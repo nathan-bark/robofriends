ff0661967f091084b5c5fdad3b35e3a5687fb62b
Nathan <nathanbark4@gmail.com>
2023-01-23 10:26:49 +0200

    introduced fetchAPI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { useState } from "react";
 import "./App.css";
-import { robots } from "./robots";
 import Cardlist from "./CardList";
-import SearchBox from './SearchBox';
+import SearchBox from "./SearchBox";
+import { useEffect } from "react";
 
 function App() {
-  const [currentRobots] = useState(robots);
-  const [searchField, setSearchField ]= useState('');
+  const [currentRobots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setRobots(users));
+  },[]);
 
   const onSearchChange = (e) => {
     setSearchField(e.target.value);
-  }
+  };
 
-  const filteredRobots = currentRobots.filter(robot => {
-    return robot.name.toLowerCase().includes(searchField.toLowerCase())
-  })
+  const filteredRobots = currentRobots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  });
 
   return (
     <div className="tc">
       <h1 className="f1 ">Robofriends</h1>
-      <SearchBox searchField={searchField} searchChange={onSearchChange}/>
+      <SearchBox searchField={searchField} searchChange={onSearchChange} />
       <Cardlist robots={filteredRobots} />;
     </div>
   );
